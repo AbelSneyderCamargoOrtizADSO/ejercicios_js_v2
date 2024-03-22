703727a5c5a2b7d84305a2935c756d9c3d784e0c
AbelSneyderCamargoOrtiz <abelc8099@gmail.com>
2024-03-22 10:29:37 -0500

    finish

--- ejercicio4.js
+++ ejercicio4.js
@@ -1,3 +1,39 @@
 /* • Crea una función para calcular cuantas silabas tiene cada palabra, imprime el resultado en la consola. */
 
+let palabra = prompt("INGRESE UNA PALABRA");
+
+function contarSilabas(palabra) {
+    palabra = palabra.toLowerCase().trim(); 
+    let numSílabas = 0;
+    let esDiptongo = false;
+
+    function esVocal(letra) {
+        return 'aeiouáéíóú'.includes(letra);
+    }
+
+
+    for (let i = 0; i < palabra.length; i++) {
+
+        if (esVocal(palabra[i])) {
+
+            if (!esDiptongo) {
+                numSílabas++;
+
+                if (i < palabra.length - 1 && esVocal(palabra[i + 1])) {
+                    let sílaba = palabra[i] + palabra[i + 1];
+
+                    if ((sílaba === 'ai' || sílaba === 'ei' || sílaba === 'oi' || sílaba === 'au' || sílaba === 'eu' || sílaba === 'iu')) {
+                        esDiptongo = true;
+                    }
+                }
+            } else {
+                esDiptongo = false;
+            }
+        }
+    }
+
+    return numSílabas;
+}
+
+alert(`LA PALABRA TIENE ${contarSilabas(palabra)} SILABAS`);
 
